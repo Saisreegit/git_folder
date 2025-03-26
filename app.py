c0f3bea46e06da876ef032e9f1bc6f27fa1e047e
Saisreegit <saisreekemburu@gmail.com>
2025-03-26 14:39:48 +0530

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, request
-import subprocess
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/github-webhook", methods=["POST"])
-def github_webhook():
-    data = request.json
-    if "ref" in data and data["ref"] == "refs/heads/main":
-        subprocess.run(["/home/ec2-user/update_flask.sh"], shell=True)
-        return "Updated Successfully", 200
-    return "No update", 400
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
