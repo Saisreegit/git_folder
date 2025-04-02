7472aa10c2074cc7492dc3764e0180d93f6ec21e
Saisreegit <saisreekemburu@gmail.com>
2025-04-02 10:59:17 +0530

    app.py

--- app.py
+++ app.py
@@ -2,9 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/service")
+def service():
+    return render_template("service.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/student_details")
+def student_details():
+    return render_template("student_details.html")
+
+@app.route("/student_marks")
+def student_marks():
+    return render_template("student_marks.html")
+
+@app.route("/student_skills")
+def student_skills():
+    return render_template("student_skills.html")
+
+@app.route("/student_poy")
+def student_poy():
+    return render_template("student_poy.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(debug=True)
